cdc0b2293087be22a048a2b7b33fd49b2cc59418
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-25 23:29:01 +0530

    Add files via upload

--- signup.js
+++ signup.js
@@ -1,54 +1,33 @@
-const express = require('express');
-const { validateSignUpData } = require('../utils/validation');
-
-const signupModel = require('../models/signup');
+const mongoose = require('mongoose');
+const validator = require("validator");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-const router = express.Router();
-
-
-router.post("/signup", async (req, res) => {
-
-    try {
-        validateSignUpData(req);
-        const { name, email, password } = req.body;
-        const passwordHash = await bcrypt.hash(password, 10);
-
-        const user = new Signup({
-            name,
-            email,
-            password: passwordHash
-        });
-        await user.save();
-        res.send("User added");
-    }
-    catch (err) {
-        res.status(400).send("ERROR:" + err.message);
+const signupSchema = new mongoose.Schema({
+    name : {
+      required : [true, 'Required field'],
+      type: String
+    },
+    email : {
+        required : [true , 'Required field'],
+        type : String
+    },
+    password : {
+        required :[ true , 'Required field'],
+        type : String
     }
 })
 
-router.post("/login", async (req, res) => {
+signupSchema.methods.getJWT = async function() {
 
-    try {
+    const user = this;
 
-        const { email, password } = req.body;
-        const user = await signupModel.findOne({ email: email });
-        if (!user) {
-            throw new Error("User Not Found");
-        }
+    const token = await jwt.sign({_id : user._id}, "Ajay@314", {expiresIn: "1h"});
 
-       
+    return token;
+}
 
-            const token = await user.getJWT();
-            console.log(token);
-            res.cookie("token", token);
-            res.send("Login Sucess");
-        
-    }
-    catch (err) {
-        res.status(400).send("ERROR:" + err.message);
-    }
-})
 
-module.exports = router;
+const signupModel = mongoose.model("signup",signupSchema);
+
+module.exports = signupModel;
